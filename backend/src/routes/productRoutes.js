f32c56e66d13c7b7b9950f463aeb505d2c387e76
Denis Neves <nevesdenis35@gmail.com>
2025-05-17 14:57:08 -0300

    Cria rota para produtos

---
+++ backend/src/routes/productRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { listProducts } from "../controllers/productController.js";
+
+const router = express.Router();
+
+router.get("/", listProducts);
+
+export default router;

d37e863cc72fb0ea9f483aeadffe528618c46aa2
Denis Neves <nevesdenis35@gmail.com>
2025-05-17 15:29:19 -0300

    Consome api de produtos

--- js/carrossel.js
+++ js/carrossel.js
@@ -2,10 +2,6 @@ const containerCard = document.querySelector(".container-card");
 const btnPrev = document.querySelector(".slider-btn.prev");
 const btnNext = document.querySelector(".slider-btn.next");
 
-console.log("containerCard: ", containerCard);
-console.log("btnNext: ", btnNext);
-console.log("btnPrev: ", btnPrev);
-
 const scrollAmount = 355;
 
 btnPrev.addEventListener("click", () => {
@@ -21,3 +17,44 @@ btnNext.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+async function loadContent() {
+  try {
+    const response = await fetch("http://localhost:3000/api/products");
+    const data = await response.json();
+    console.log(data);
+
+    data.forEach((product) => {
+      const cardHTML = `
+        <div class="card-produto">
+          <img
+            src="${product.image}"
+            alt="${product.name}"
+            width="358px"
+            height="356px"
+          />
+          <h3>${product.name}</h3>
+          <p>${product.description}</p>
+          
+          <div class="button-container">
+            <button class="remove" aria-label="Botão de remover">
+              <i class="fi fi-rr-minus"></i>
+            </button>
+
+            <p>Comprar</p>
+
+            <button class="add" aria-label="Botão de adicionar">
+              <i class="fi fi-rr-add"></i>
+            </button>
+          </div>
+        </div>
+      `;
+
+      containerCard.insertAdjacentHTML("beforeend", cardHTML);
+    });
+  } catch (error) {
+    console.log("Falha ao carregar o conteudo: " + error);
+  }
+}
+
+loadContent();
